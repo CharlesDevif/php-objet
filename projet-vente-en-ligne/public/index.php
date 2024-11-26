a85d82681e0df4923613094b2971502cc5dc0569
Snowsdy <67746810+Snowsdy@users.noreply.github.com>
2024-11-26 14:13:07 +0100

    🔥 Debut IHM (#1)
    * 🔥 Debut IHM
    
    Supression de version dans le docker-compose car plus utile
    
    * 📝 Documentation à jour
    
    * 🚧 section ajout produits

--- projet-vente-en-ligne/public/index.php
+++ projet-vente-en-ligne/public/index.php
@@ -113,4 +113,101 @@ echo "Commission : " . $vendeur->getCommission() . "%\n";
 
 // Le vendeur ajoute un produit (fonctionnalité à implémenter)
 $vendeur->ajouterProduit($produitPhysique);
+?>
 
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDSI PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container p-4">
+        <div class="row gap-5">
+            <h2>Ajout de produits</h2>
+
+            <!-- ProduitNumerique -->
+            <div class="col card p-3">
+                <h3>Ajout Produit Numérique</h3>
+                <form>
+                    <div class="mb-3">
+                        <label for="inputNomProduit" class="form-label">Nom du produit :</label>
+                        <input type="text" class="form-control" id="inputNomProduit">
+                    </div>
+                    <div class="mb-3 form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="textAreaDescription"></textarea>
+                        <label for="textAreaDescription">Description</label>
+                    </div>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" id="inputPrixProduit" placeholder="Prix HT" aria-label="Montant (à l'euro près)">
+                        <span class="input-group-text">€</span>
+                    </div>
+                    <div class="mb-2">
+                        <label for="numberStock" class="form-label">Stock</label>
+                        <input type="number" id="numberStock" class="form-control" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Sélectionner le fichier à importer.</label>
+                        <input class="form-control" type="file" id="formFile">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ajouter</button>
+                </form>
+            </div>
+
+            <!-- ProduitPerissable -->
+            <div class="col card p-3">
+                <h3>Ajout Produit Périssable</h3>
+                <form>
+                    <div class="mb-3">
+                        <label for="inputNomProduit" class="form-label">Nom du produit :</label>
+                        <input type="text" class="form-control" id="inputNomProduit">
+                    </div>
+                    <div class="mb-3 form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="textAreaDescription"></textarea>
+                        <label for="textAreaDescription">Description</label>
+                    </div>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" id="inputPrixProduit" placeholder="Prix HT" aria-label="Montant (à l'euro près)">
+                        <span class="input-group-text">€</span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="numberStock" class="form-label">Stock</label>
+                        <input type="number" id="numberStock" class="form-control" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ajouter</button>
+                </form>
+            </div>
+
+            <!-- ProduitPhysique -->
+            <div class="col card p-3">
+                <h3>Ajout Produit Physique</h3>
+                <form>
+                    <div class="mb-3">
+                        <label for="inputNomProduit" class="form-label">Nom du produit :</label>
+                        <input type="text" class="form-control" id="inputNomProduit">
+                    </div>
+                    <div class="mb-3 form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="textAreaDescription"></textarea>
+                        <label for="textAreaDescription">Description</label>
+                    </div>
+                    <div class="input-group mb-1">
+                        <input type="text" class="form-control" id="inputPrixProduit" placeholder="Prix HT" aria-label="Montant (à l'euro près)">
+                        <span class="input-group-text">€</span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="numberStock" class="form-label">Stock</label>
+                        <input type="number" id="numberStock" class="form-control" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ajouter</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
