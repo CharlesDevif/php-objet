1d41bdc425a1e931acba11d82d37cba3ee755fb5
Snowsdy <shadowss.yt@gmail.com>
2024-11-28 10:14:30 +0100

    ✨ Ajout variable global dans balise title

--- projet-vente-en-ligne/src/Views/default.php
+++ projet-vente-en-ligne/src/Views/default.php
@@ -4,13 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Application - Accueil</title>
+    <title>Mon Application - <?= isset($title) ? $title : "Accueil" ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <header>
-        <h1>Bienvenue sur Mon Application</h1>
         <!-- Ajoutez un menu de navigation si nécessaire -->
     </header>
 
